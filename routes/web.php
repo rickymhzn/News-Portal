034e8cd8ae335e07931df118c59a6f071aa3d6ab
AptechArena <TeamdevAptech@gmail.com>
2019-10-22 15:50:39 +0545

    Frontend

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,23 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Auth::routes();
+Route::group(['prefix'=>'back'],function(){
+    Route::get('/','Admin\DashboardController@index')->name('dashbaord');
+    Route::get('/category','Admin\CategoryController@index')->name('category');
+    Route::get('/category/create','Admin\CategoryController@create');
+    Route::get('/category/edit','Admin\CategoryController@edit');
+
+
 });
+
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/test', function(){
+    return view('frontend.layouts.master');
+});
+;
+//Frontend Controllers
+Route::get('/','HomePageController@index');
+Route::get('/listing','ListingPageController@index');
+Route::get('/details','DetailsPageController@index');
