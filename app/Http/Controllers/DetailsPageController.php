82c89643d44a9daacd5633103ca3d41fd2f97295
AptechArena <TeamdevAptech@gmail.com>
2019-11-19 12:04:48 +0545

    Post Details Management

--- app/Http/Controllers/DetailsPageController.php
+++ app/Http/Controllers/DetailsPageController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Post;
 use Illuminate\Http\Request;
 
 class DetailsPageController extends Controller
 {
-    public function index()
+    public function index($slug)
     {
-        return view('frontend.details');
+        $post = Post::with(['creator','category','comments'])->where('status',1)->where('slug',$slug)->first();
+        $post->view_count = $post->view_count+1;
+        $related_news = Post::with(['creator','category','comments'])->where('status',1)->where('id','!=',$post->id)->where('category_id',$post->category_id)->orderBy('id','DESC')->limit(4)->get();
+        $post->save();
+        return view('frontend.details',compact('post','related_news'));
     }
 }
--- app/Http/Controllers/ListingPageController.php
+++ app/Http/Controllers/ListingPageController.php
@@ -13,7 +13,7 @@ class ListingPageController extends Controller
     }
     public function listing($id)
     {
-        $posts = Post::with(['comments','category','creator'])->where('status',1)->where('category_id',$id)->orWhere('created_by',$id)->orderBy('id','DESC')->paginate(3);
+        $posts = Post::with(['comments','category','creator'])->where('status',1)->where('category_id',$id)->orWhere('created_by',$id)->orderBy('id','DESC')->paginate(5);
 
         return view('frontend.listing',compact('posts'));
     }
